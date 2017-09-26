538d199f545d8595d7d5d61b8d35d190e067168c
Ryan McCarty <rmac_whf@yahoo.com>
2017-09-26 11:22:57 -0400

    first commit

---
+++ BandSite/BandSite/Models/BillingAddress.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandSite.Models
+{
+    public class BillingAddress
+    {
+        [Key]
+        public int billingAddressId { get; set; }
+
+        [Required]
+        public string street { get; set; }
+
+        public int? unitNumber { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Valid Zip is Required")]
+        [RegularExpression(@"^\d{5}(-\d{4})?$", ErrorMessage = "Invalid Zip Code")]
+        public int ZipCode { get; set; }
+
+        public bool isDefault { get; set; }
+
+        [Required]
+        public ApplicationUser user { get; set; }
+
+        public virtual ICollection<Order> Order { get; set; }
+
+
+    }
+}
---
+++ BandSite/BandSite/Models/ProductType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandSite.Models
+{
+    public class ProductType
+    {
+    }
+}
