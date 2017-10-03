b37387c48c68723d19c6b2c764c4642079e4eb19
Ryan McCarty <rmac_whf@yahoo.com>
2017-10-03 17:07:23 -0400

    saving things

---
+++ BandSite/BandSite/Migrations/20171003204359_fourth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BandSite.Data.Migrations
+{
+    public partial class fourth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    MemberId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Bio = table.Column<string>(nullable: false),
+                    Facebook = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Twitter = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.MemberId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Members");
+        }
+    }
+}
